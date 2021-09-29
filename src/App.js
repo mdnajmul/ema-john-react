7f337b041eba2c52638fd1bb445aad2ebcc112de
mdnajmul <mdnajmulovi@gmail.com>
2021-09-29 09:22:42 +0600

    favicon && logo update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./images/logo.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img style={{ width: "250px", marginTop: "10px" }} src={logo} alt="" />
     </div>
   );
 }
