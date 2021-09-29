c21bf009a49b61d4b15bf144faa451abae5d84d4
mdnajmul <mdnajmulovi@gmail.com>
2021-09-29 19:56:29 +0600

    header menu part complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 import logo from "./images/logo.png";
 
 function App() {
   return (
     <div className="App">
       <img style={{ width: "250px", marginTop: "10px" }} src={logo} alt="" />
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header-menu">
+      <nav>
+        <a href="#">Shop</a>
+        <a href="#">Order Review</a>
+        <a href="#">Manage Inventory here</a>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
